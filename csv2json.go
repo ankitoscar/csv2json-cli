f51945d12b98975562a55aa54f5f095662396832
ankitoscar <ankitoscar911@gmail.com>
2021-12-21 20:17:55 +0530

    Added main function code

--- csv2json.go
+++ csv2json.go
@@ -181,5 +181,26 @@ func getJsonFunc(pretty bool) (func(map[string]string) string, string) {
 }
 
 func main() {
-	// fileData, err := getFileData()
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options] <csvFile>\nOptions:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	fileData, err := getFileData()
+
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	if _, err := checkIfValidFile(fileData.filepath); err != nil {
+		exitGracefully(err)
+	}
+
+	writerChannel := make(chan map[string]string)
+	done := make(chan bool)
+
+	go processCsvFile(fileData, writerChannel)
+	go writeJSONFile(fileData.filepath, writerChannel, done, fileData.pretty)
+
+	<-done
 }
